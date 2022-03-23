cb91d3b534ce3e5dafbb6410669274f06da44b88
deminovamv <deminovamv@gmail.com>
2022-03-23 16:25:27 +0700

    new version

---
+++ sum_numbers_v2.py
@@ -0,0 +1,29 @@
+"""Разработайте приложение принимающее на вход два числа и
+выводящее сумму этих чисел"""
+
+
+class NumberOperation:
+    def __init__(self, first_number, second_number):
+        self.first_number = first_number
+        self.second_number = second_number
+
+    def print_info(self):
+        try:
+            first_number = int(self.first_number)
+            second_number = int(self.second_number)
+            print(
+                "{} + {} = {}".format(
+                    self.first_number,
+                    self.second_number,
+                    first_number + second_number,
+                )
+            )
+        except ValueError:
+            print("Вы ввели слова, мне нужны числа")
+
+
+if __name__ == "__main__":
+    user = NumberOperation(
+        input("Введите первое число: "), input("Введите второе число: ")
+    )
+    user.print_info()
