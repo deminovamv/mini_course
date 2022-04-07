18a63da2d116d8a9c65a483f4d25054f4c2f1474
deminovamv <deminovamv@gmail.com>
2022-04-07 20:26:18 +0300

    change 3 tasks

--- Lection 4/hw_3.py
+++ Lection 4/hw_3.py
@@ -1,13 +1,14 @@
 # Напишите программу которая сортирует список кортежей людей по их росту
 # Функция не должна ничего возвращать, а менять список "in-place"
-from operator import itemgetter
+# from operator import itemgetter
 
 LST = [("Alice", 160), ("John", 180), ("Karen", 163), ("Michael", 182), ("Peter", 172)]
 
 
 def sorted_by_height():
     global LST
-    LST.sort(key=itemgetter(1), reverse=True)
+  # LST.sort(key=itemgetter(1), reverse=True)
+    LST.sort(key=lambda height: height[1], reverse=True)
 
 
 if __name__ == "__main__":
